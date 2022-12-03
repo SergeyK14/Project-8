55f3b571536a0833d8d19442e85f77fd100cf950
SergeyK <serega_konchakov@mail.ru>
2022-12-03 18:27:32 +0300

    Количество четных элементов в случайном массиве из трехзначных чисел

---
+++ Сумма положительных элементов в массиве со значениями от 100 до 999/Program.cs
@@ -0,0 +1,38 @@
+﻿// Количество четных чисел в случайном массиве 
+//из трехзначных положительных чисел
+int size = 11;
+int[] numbers = new int[size];
+
+void FillArray(int[] nums)
+{
+Random rand = new Random();
+int length = nums.Length;
+    for (int i = 0; i < length; i++)
+    {
+    nums[i] = rand.Next (100, 1000);
+    }
+}
+void PrintArray(int[] nums)
+{   
+int length = nums.Length;
+Console.WriteLine ("Массив: ");
+    for (int i = 0; i < length; i++)
+    {
+    Console.Write (nums[i] + " ");
+    }
+Console.WriteLine();
+Console.WriteLine();
+}
+
+FillArray(numbers);
+PrintArray(numbers);
+
+int sum = 0;
+for (int i = 0; i < size; i ++)
+{
+    if (numbers[i]%2 == 0)
+    {
+        sum += 1;
+    }
+}
+Console.WriteLine ("Количество четных элементов в массиве = " + sum);
